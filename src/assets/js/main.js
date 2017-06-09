47473dc31631d90f1c4b3e716da71e6ed65c1af4
E64 <E64@portable-E64-phnou-2.local>
2017-06-10 01:21:07 +0200

    surbrillance fleches au survol Intégration less

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -5,4 +5,18 @@ $(document).ready(function () {
     $('.arrow.right').on("click", function () {
         $("#pages").dragend("left");
     });
+
+    $('.arrow.left').on("mouseenter", function () {
+        $(this).find('.arrowColor').addClass('arrowColorLighthover');
+    });
+    $('.arrow.right').on("mouseenter", function () {
+        $(this).find('.arrowColor').addClass('arrowColorLighthover');
+    });
+
+    $('.arrow.left').on("mouseleave", function () {
+        $(this).find('.arrowColor').removeClass('arrowColorLighthover');
+    });
+    $('.arrow.right').on("mouseleave", function () {
+        $(this).find('.arrowColor').removeClass('arrowColorLighthover');
+    });
 });
