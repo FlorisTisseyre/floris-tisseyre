9d75cf794726419e305afb0402491ff1bf520b82
FlorisTisseyre <f.tissyre@gmail.com>
2017-06-25 22:34:47 +0200

    Style d'impression

--- index.php
+++ index.php
@@ -1,27 +1,29 @@
 <!DOCTYPE HTML>
 <html>
 <head>
-	<meta charset="utf-8"/>
-    <meta name="keywords" content="floris, tisseyre, tisseire, tysseire, cv, curriculum, vitae, parcours, profil, perso, agile, manager, management, reseau, informatique, développement">
+    <meta charset="utf-8"/>
+    <meta name="keywords"
+          content="floris, tisseyre, tisseire, tysseire, cv, curriculum, vitae, parcours, profil, perso, agile, manager, management, reseau, informatique, développement">
     <title>CV Floris Tisseyre</title>
-	<link rel="stylesheet" href="semantic/dist/semantic.css"/>
-	<link rel="stylesheet" href="src/assets/css/main.css"/>
-	<link rel="stylesheet" href="node_modules/jqcloud2/dist/jqcloud.min.css"/>
-	<script
-		src="https://code.jquery.com/jquery-3.1.1.min.js"
-		integrity="sha256-hVVnYaiADRTO2PzUGmuLJr8BLUSjGIZsDYGmIJLv2b8="
-		crossorigin="anonymous"></script>
-	<script src="semantic/dist/semantic.js"></script>
+    <link rel="stylesheet" href="semantic/dist/semantic.css"/>
+    <link rel="stylesheet" href="src/assets/css/main.css"/>
+    <link rel="stylesheet" type="text/css" href="src/assets/css/print.css" media="print" />
+    <link rel="stylesheet" href="node_modules/jqcloud2/dist/jqcloud.min.css"/>
+    <script
+            src="https://code.jquery.com/jquery-3.1.1.min.js"
+            integrity="sha256-hVVnYaiADRTO2PzUGmuLJr8BLUSjGIZsDYGmIJLv2b8="
+            crossorigin="anonymous"></script>
+    <script src="semantic/dist/semantic.js"></script>
     <script src="node_modules/snapsvg/dist/snap.svg-min.js"></script>
     <script src="node_modules/jqcloud2/dist/jqcloud.min.js"></script>
-	<script src="src/assets/js/main.js"></script>
+    <script src="src/assets/js/main.js"></script>
 </head>
 <body>
+
 <div class="container" id="container">
-    <div id="illustration"><?php include __DIR__.'/src/assets/pictos/illustration2.svg'; ?></div>
+    <div id="illustration"><?php include __DIR__ . '/src/assets/pictos/illustration2.svg'; ?></div>
 </div>
 <?php include __DIR__ . '/src/mainContent.php'; ?>
-
-
+<div class="ui massive blue message" id="web-link">Plus d'infos sur www.floris-tisseyre.fr</div>
 </body>
 </html>
