d88c31ec9c7878d588a2fcb89c9e1050aecb6884
FlorisTisseyre <f.tissyre@gmail.com>
2017-10-21 21:23:16 +0200

    Ajout du lanceur de langue

---
+++ src/launcher.php
@@ -0,0 +1,38 @@
+<div class="container" id="container">
+    <div class="ui small circular rotate reveal image" id="photoFloris">
+        <img src="src/images/avatar-bonjour-carre.jpg" class="visible content">
+        <img src="src/images/floris-carre.jpg" class="hidden content">
+    </div>
+
+    <div id="identite">
+        <h1 class="ui header">Floris Tisseyre</h1>
+        <h2 class="ui header">Manager 3.0</h2>
+    </div>
+
+    <div class="ui header">
+        <div class="ui centered grid">
+            <a href="?lang=fr">
+                <div>
+                    <i class="huge icons">
+                        <i class="play icon"></i>
+                        <i class="corner home icon"></i>
+                    </i>
+                </div>
+                <div>
+                    Français
+                </div>
+            </a>
+            
+            <a href="?lang=en">
+                <div>
+                <i class="huge icons">
+                    <i class="play icon"></i>
+                    <i class="corner world icon"></i>
+                </i>
+                </div>
+                <div>
+                    English
+                </div>
+        </div>
+    </div>
+</div>
--- src/mainContent.php
+++ src/mainContent.php
@@ -1,31 +1,17 @@
-<div id="mainContent">
-    
-    <?php
-    if(isset($_GET['lang']) && $_GET['lang'] === 'en') {
-        $lang = 'en';
-    } else {
-        $lang = 'fr';
-    }
-    ?>
-    
-    <div class="container" id="container">
-        <div id="illustration"><?php include __DIR__ . '/assets/pictos/illustration.svg'; ?></div>
+<div class="ui grid">
+    <div class="two wide column">
     </div>
-    
-	<?php include __DIR__ . "/articles/$lang/motiver.php"; ?>
-	<?php include __DIR__ . "/articles/$lang/unir.php"; ?>
-	<?php include __DIR__ . "/articles/$lang/innover.php"; ?>
-	<?php include __DIR__ . "/articles/$lang/evoluer.php"; ?>
-	<?php include __DIR__ . "/experiences/$lang/diplome.php"; ?>
-	<?php include __DIR__ . "/experiences/$lang/developpeur.php"; ?>
-	<?php include __DIR__ . "/experiences/$lang/chefprojet.php"; ?>
-	<?php include __DIR__ . "/experiences/$lang/changeleader.php"; ?>
-	<?php include __DIR__ . "/experiences/$lang/itmanager.php"; ?>
-
-    <div class="ui massive blue message" id="web-link">
-        <?php
-            echo ($lang === "en") ? "More info on www.floris-tisseyre.fr" : "Plus d'infos sur www.floris-tisseyre.fr";
-        ?>
+    <div class="twelve wide column">
+            <div id="mainContent">
+            <?php
+            if (!isset($_GET['lang'])) {
+                include __DIR__ . '/launcher.php';
+            } else {
+                include __DIR__ . '/resume.php';
+            }
+            ?>
+            </div>
+    </div>
+    <div class="column">
     </div>
 </div>
-
